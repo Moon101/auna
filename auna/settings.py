db9f5aeedf00ec8cd97340bb9ab91a3bc798be7e
Jordi L <recavap@gmail.com>
2025-03-09 02:59:28 +0100

    Inicial con idiomas correcto

--- auna/settings.py
+++ auna/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # Jazzmin debe ir antes de 'django.contrib.admin'
     'jazzmin',  # Para personalizar el admin
-    
+
     # Aplicaciones predeterminadas de Django
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auna',
 
     # Librerías instaladas
     'import_export',  # Para importar/exportar datos
@@ -337,7 +338,7 @@ JAZZMIN_SETTINGS = {
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
     # for the full list of 5.13.0 free icon classes
     "icons": {
-	    "auth": "fas fa-users-cog",
+        "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
         "auth.group": "fas fa-users",
         "intranet_app.Socio": "fas fa-user-friends",
